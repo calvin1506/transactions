f893e15bb7e0736f43702c342a7b57ae93da824d
calvin <calvin1506@gmail.com>
2021-09-16 22:16:56 +0700

    ganti id table operator, salah sebelumnya

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -312,8 +312,8 @@
 </div>
 <!-- Modal 3 -->
 <div class="modal fade" id="modal3" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
+    <div class="modal-dialog modal-dialog-besar" role="document">
+        <div class="modal-content modal-content-besar">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Operators</h5>
             </div>
@@ -326,7 +326,7 @@
                 </div>
                 <div class="row mt-3">
                     <div class="col-md-12">
-                        <table class="table table-striped text-center" id="tbl_leader_list">
+                        <table class="table table-striped text-center" id="tbl_operator_list">
                             <thead>
                                 <tr>
                                     <td>No</td>
